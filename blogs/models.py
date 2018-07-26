a07c527eab558ae9170ea03c2ffe08b5ff31e18d
Nikolay Pogoreliy <Nikolay.Pogoreliy@skypro.ch>
2018-07-26 18:52:40 +0300

    initial

---
+++ blogs/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Posts(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    is_published = models.BooleanField(default=False)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Posts'
+        ordering = ['created']
+
+
+class Comments(models.Model):
+    post = models.ForeignKey(to=Posts, on_delete=models.CASCADE)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now=True)
+    parrent_id = models.IntegerField(null=True, default=None)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text[:20]
+
+    class Meta:
+        ordering = ['-created']
